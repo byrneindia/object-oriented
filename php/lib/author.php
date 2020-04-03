d89f287b77d7f469bb59dae7a1fa1d35bad21c34
India Byrne <byrneindia@gmail.com>
2020-04-03 11:29:37 -0600

    Finally update fixed syntax.

--- php/lib/author.php
+++ php/lib/author.php
@@ -7,8 +7,8 @@ use byrneindia\objectoriented\author;
 //use the constructor
 $robinson = new Author("07f2595c-75d0-11ea-bc55-0242ac130003",
 	"13b7ff8a75d011eabc550242ac130003",
-	"byrneindia.edu",
-	"deepdive-coders.edu",
+	"byrneindia@.edu",
+	"deepdive-coders@.edu",
 	"2508b78e6df07bd85670289f7a5a86b0eacd45c33d6c33b120221e84051c482bbd23296a95f01340665dcb0969cc00489",
 	"robinson",);
 echo("Author ID: ");
