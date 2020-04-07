e30d0f523baa0d2c36a377b1e74f5188ef2ede24
India Byrne <byrneindia@gmail.com>
2020-04-07 17:08:53 -0600

    Updated namespace.

---
+++ Marty's Author Class/Example.php
@@ -0,0 +1,8 @@
+<?php
+
+$something = ["firstThing"=>"asdfs", "anotherThing" =>7, "lastThing"=>"hello"];
+$something ["firstThing"];
+
+$somethingMoreComplex = {"color": "red", "wheels": 4};
+$somethingMoreComplex->color;
+
