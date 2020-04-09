7e95e8e81458f6653a061decf75cb5ed9a71ce9c
India Byrne <byrneindia@gmail.com>
2020-04-09 14:19:34 -0600

    Testing lecture code along.

--- php/Classes/Test/AuthorTest.php
+++ php/Classes/Test/AuthorTest.php
@@ -1,1 +1,15 @@
 <?php
+namespace DeepDiveDylan\DataDesign\Test;
+
+use Deepdivedylan\DataDesign\{Profile, Tweet};
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+
+class TweetTest extends DataDesignTest {
+
+}
